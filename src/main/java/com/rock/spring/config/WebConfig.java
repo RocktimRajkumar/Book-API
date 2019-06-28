dee31df4476316e04890562365864766857eb180
Rocktim <rajkumar.rocktim@gmail.com>
2019-06-28 08:16:17 +0530

    MyWebAppInitializer and WebConfig configuration added

---
+++ src/main/java/com/rock/spring/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.rock.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.rock.spring.controller"})
+
+public class WebConfig implements WebMvcConfigurer{
+
+}
